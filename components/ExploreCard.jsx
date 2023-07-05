10a8e75b363a70565530767ad54f5777b3acd1b7
Oliver Yao <53160804+OliverYao09@users.noreply.github.com>
2023-07-05 16:15:34 +1000

    bug/ fix a bug where the explore section has a issue on mobile screen.

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -15,7 +15,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   >
     <img
       src={imgUrl}
-      alt={title}
+      alt="planet-04"
       className="absolute w-full h-full object-cover rounded-[24px]"
     />
     {active !== id ? (
@@ -23,7 +23,7 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
         {title}
       </h3>
     ) : (
-      <div className="absolute bottom-0 p-8 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
+      <div className="absolute bottom-0 p-8 flex justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px]">
         <div
           className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
         >
@@ -33,14 +33,11 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
             className="w-1/2 h-1/2 object-contain"
           />
         </div>
-        <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
-          Enter the metaverse
+        <p className="font-normal text-[16px] leading-[20.16px] text-white uppercase">
+          Enter Metaverse
         </p>
-        <h2
-          className="mt-[24px] font-semibold sm:text-[32px]
-          text-[24px] text-white"
-        >
-          The Upside Down
+        <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
+          {title}
         </h2>
       </div>
     )}
