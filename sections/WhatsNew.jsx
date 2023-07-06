fd5349291458e527cf809b6881af08d71fcbfde0
Oliver Yao <53160804+OliverYao09@users.noreply.github.com>
2023-07-06 14:29:24 +1000

    fix: fix padding issue in What's New section

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -3,9 +3,9 @@
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
-import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
-import { NewFeatures, TitleText, TypingText } from '../components';
 import { newFeatures } from '../constants';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import { planetVariants, staggerContainer, fadeIn } from '../utils/motion';
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -17,24 +17,25 @@ const WhatsNew = () => (
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
-        variants={fadeIn('left', 'tween', 0.2, 1)}
-        className="flex-[0.75] flex justify-center flex-col"
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.95] flex justify-center flex-col"
       >
-        <TypingText title="| What's New?" />
-        <TitleText title={<>What's new about Metaverse?</>} />
+        <TypingText title="| Whats new?" />
+        <TitleText title={<>What's new about Metaversus?</>} />
         <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
           {newFeatures.map((feature) => (
             <NewFeatures key={feature.title} {...feature} />
           ))}
         </div>
       </motion.div>
+
       <motion.div
         variants={planetVariants('right')}
         className={`flex-1 ${styles.flexCenter}`}
       >
         <img
           src="/whats-new.png"
-          alt="whats new"
+          alt="get-started"
           className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
